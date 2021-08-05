28587623bc2d3c356287e2a285e666391ed28bbc
bcp2415 <64213121+bcp2415@users.noreply.github.com>
2021-08-05 11:37:03 -0500

    Adds code to store each note using localStorage.

--- script.js
+++ script.js
@@ -1,9 +1,25 @@
-console.log('Testing...')
-
 // Declare some variables:
-var dummy;
+var note1 = document.querySelector('#note1');
+var addBtn = document.querySelector('#addBtn');
 
 // fcn to accept input for a note:
+addBtn.addEventListener('click', function() {
+
+    // Read entry from textarea element:
+    var txt = note1.value;
+
+    // Store in memory
+    var nextNote = 1;
+    nextNote++;
+    var currentNote = `note${nextNote}`;
+    localStorage.setItem(currentNote, txt);
+
+    // Test storage function:
+    console.log(localStorage.getItem(currentNote));
+
+    // Display note on screen 
+
+});
 
 // fcn to delete a note
 
